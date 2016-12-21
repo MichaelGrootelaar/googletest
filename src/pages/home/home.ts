6874d88e4654310869925b3512ca10177eee05b8
root <m13grootelaar@gmail.com>
2016-12-21 10:00:37 +0100

    commit 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  template: `
+    <ion-content padding>
+      <ion-card *ngIf="userData">
+        <ioncard-content>
+          <img class="profile-img" [hidden]="!userData" [src]="userData.imageUrl" />
+          <h1>{{userData.displayName}}</h1>
+          <p>{{userData.email}}</p>
+        </ion-card-content>
+      </ion-card>
+      <button (click)="loginUser()">Login</button>
+      <button (click)="logoutUser()">Login</button>
+    </ion-content>
+  `
 })
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  public userData;
 }
