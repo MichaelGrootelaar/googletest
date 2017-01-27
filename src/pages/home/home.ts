6be6703fb7273a337bef977e96b94d61e0fe091f
root <m13grootelaar@gmail.com>
2017-01-27 10:28:31 +0100

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
+import { GooglePlus } from 'ionic-native';
 
 @Component({
   template: `
     <ion-content padding>
       <ion-card *ngIf="userData">
-        <ioncard-content>
+        <ion-card-content>
           <img class="profile-img" [hidden]="!userData" [src]="userData.imageUrl" />
           <h1>{{userData.displayName}}</h1>
           <p>{{userData.email}}</p>
         </ion-card-content>
       </ion-card>
       <button (click)="loginUser()">Login</button>
-      <button (click)="logoutUser()">Login</button>
+      <button (click)="logoutUser()">Logout</button>
     </ion-content>
   `
 })
 
 export class HomePage {
   public userData;
+
+  loginUser() {
+    GooglePlus.login({})
+      .then(
+        (res) => {
+          console.log('good');
+          this.userData = res;
+        },
+        (err) => {
+          console.log('error');
+          console.log(err);
+        }
+      );
+  }
+
+  logoutUser() {
+    GooglePlus.logout().then(() => this.userData = null);
+  }
 }
